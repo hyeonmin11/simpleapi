c9845e0df3a57b02a6ec897dee0895cbcd7e2bbe
hyeonmin11 <79415795+hyeonmin11@users.noreply.github.com>
2023-01-18 18:02:00 +0900

    Add files via upload

---
+++ nodejs/cookie.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var cookie = require('cookie');
+http.createServer(function(request, response){
+    
+    console.log(request.headers.cookie);
+    var cookies = {};
+    if(request.headers.cookie !== undefined){
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    
+    console.log(cookies.yummy_cookie);
+    response.writeHead(200, {
+        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry', `Permanent=cookies; Max-Ages=${60*60*24*30}`, 
+                        'Secure=Secure; Secure',
+                        'HttpOnly=HttpOnly; HttpOnly',
+                        'Path=Path; Path=/cookie',
+                    'DOmain=Domain; Domain=o2.org'] //30days
+    });
+    response.end('Cookie');
+}).listen(3000);
